170f77cb918ba03abf6b95a3f3f472da71fcd96a
Siddhant <siddhantgk01@gmail.com>
2023-02-09 20:48:20 +0530

    First Commit

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -2,4 +2,14 @@ package com.driver;
 
 public class Main {
 
+    public static void main(String[] args){
+
+        Product p = new Product();
+
+        p.product(3,5);
+        p.product(3,5,10);
+        p.product(5,1);
+
+    }
+
 }
