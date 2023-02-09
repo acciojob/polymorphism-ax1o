2c6ef9efd60c2db4ff7e773c6a6d0fbd6169f5fe
Siddhant <siddhantgk01@gmail.com>
2023-02-09 20:58:26 +0530

    Second Attempt

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -2,7 +2,22 @@ package com.driver;
 
 public class Main {
 
-    public static void main(String[] args){
+    class Product{
+        public int product(int x , int y){
+            return x*y;
+        }
+
+        public int product(int x , int y , int z){
+            return x*y*z;
+        }
+
+        public double product(double x, double y){
+            return x*y;
+        }
+    }
+
+    public void main(String[] args){
+
 
         Product p = new Product();
 
